372c3fc5b78e07f2b94449fdaebc94030d0cd829
kerenskybr <roger.o.monteiro@gmail.com>
2019-02-27 00:24:35 -0300

    Modified scraper elpais to get all text from news

--- scraper/noticia_verdadeira/scraper_elpais.py
+++ scraper/noticia_verdadeira/scraper_elpais.py
@@ -2,27 +2,106 @@ from bs4 import BeautifulSoup
 
 from urllib.request import urlopen
 
-link = urlopen("https://brasil.elpais.com/seccion/tecnologia/29")
+import requests
+import time
+import csv
 
-soup = BeautifulSoup(link, "html.parser")
+import pandas as pd
+
+#link = urlopen("https://brasil.elpais.com/seccion/tecnologia")
+
+#soup = BeautifulSoup(link, "html.parser")
+
+headers = {'User-Agent': 'Py_Scraper'}
+
+link = "https://brasil.elpais.com/seccion/tecnologia" 
+
+lista_titulo = []
+lista_corpo = []
+lista_url = []
+
+var = True
+pagina = 3 #30
+
+lista = []
+
+print('Inicializando...')
 
 
-for titulo in soup.find_all('h2',{'class':'articulo-titulo'}):
-    print('TITULO:', titulo.getText())
+while var:
 
-    for end in titulo.children:
-        print('ENDEREÇO')
-        print('https:' + end.get('href'))
+    endereco = str(link) + '/' + str(pagina)
 
-        visita = 'https:' + end.get('href')
-        #print('VISITA', visita)
+    resposta = requests.get(endereco, headers=headers)    
 
-        link_2 = urlopen(str(visita))
-        #print('LINK', link)
+    soup = BeautifulSoup(resposta.text, 'html.parser')
+   
+    try:
+        for titulo in soup.find_all('h2',{'class':'articulo-titulo'}):
+            try:
 
-        soup_2 = BeautifulSoup(link_2, "html.parser")
+                print('TITULO:', titulo.getText())
+                lista_titulo.append(titulo.getText())
+            except:
+                lista_titulo.append('NaN')
 
-        for corpo in soup_2.find_all('div', {'id':'cuerpo_noticia'}):
-            print('Terceiro loop')
-            print('CORPO', corpo.getText().split())
-            
+            for end in titulo.children:
+                try:
+                    print('ENDEREÇO')
+                    print('https:' + end.get('href'))
+
+                    visita = 'https:' + end.get('href')
+                    lista_url.append(visita)
+                except:
+                    lista_url.append('NaN')
+                
+                
+                #Coloca o link da materia em uma var nova
+                link_2 = urlopen(str(visita))
+                #Faz o parser da nova url
+                soup_2 = BeautifulSoup(link_2, "html.parser")
+
+                for corpo in soup_2.find_all('div', {'id':'cuerpo_noticia'}):
+                    try:
+                        print('CORPO', corpo.getText().replace('\n',''))
+                        lista_corpo.append(corpo.getText().replace('\n', ''))
+                    except:
+                        lista_corpo.append('NaN')
+       
+    except:
+        continue
+        '''
+        Caso algo saia errado ou a página chegue
+        ao final, o arquivo é gravado
+        '''
+    print(endereco)
+    print('##########################PAGINA##################', pagina)
+          
+    pagina-=1
+
+    if pagina <= 0:
+        break
+
+
+
+
+#listass.to_csv('lista.csv')
+'''
+csv = pd.DataFrame()
+
+csv['titulo'] = lista_titulo
+csv['corpo'] = lista_corpo
+csv['url'] = lista_url
+
+csv.to_csv('el_pais_full.csv')
+
+
+
+'''
+s1 = pd.Series(lista_titulo, name='titulo')
+s2 = pd.Series(lista_corpo, name='corpo')
+s3 = pd.Series(lista_url, name='url')
+
+s1.to_csv('s1.csv')
+s2.to_csv('s2.csv')
+s3.to_csv('s3.csv')
